012095c355f3646ae97a334a5e5842ee05e79cfe
23prime <23.prime.37@gmail.com>
2023-06-23 00:04:48 +0900

    Impl hello world app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, routing::get, Json, Router};
+use serde::Serialize;
+
+#[tokio::main]
+async fn main() {
+    let host = "0.0.0.0";
+    let port = "3000";
+
+    let app = Router::new().route("/", get(root));
+
+    axum::Server::bind(&format!("{}:{}", host, port).parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+#[derive(Debug, Serialize)]
+struct Hello {
+    message: String,
+}
+
+impl Hello {
+    fn new(message: &str) -> Self {
+        Hello {
+            message: message.to_string(),
+        }
+    }
+}
+
+async fn root() -> (StatusCode, Json<Hello>) {
+    return (StatusCode::OK, Json(Hello::new("Hello, World!")));
 }
